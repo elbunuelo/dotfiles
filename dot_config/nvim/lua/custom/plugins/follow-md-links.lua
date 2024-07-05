5c6fa59d6eb8ef97c7a913d92325f2c5b9d8d921
Nicolas Arias-Gonzalez <nariasgonzalez@aha.io>
2024-07-05 10:50:30 -0500

    Enable link opening

---
+++ dot_config/nvim/lua/custom/plugins/follow-md-links.lua
@@ -0,0 +1,10 @@
+-- Plugin to open links in markdown files by pressing enter
+-- https://github.com/jghauser/follow-md-links.nvim
+
+-- For some
+vim.keymap.set('n', '<leader><cr>', '<Cmd>lua require("follow-md-links").follow_link()<cr>',
+  { silent = 'true', noremap = 'true', desc = 'Open link under cursor' })
+
+return {
+  'jghauser/follow-md-links.nvim'
+}
