bdcf56364ed20dcc43ed9a20369ba9a5041f8d65
Nicolas Arias-Gonzalez <nariasgonzalez@aha.io>
2024-11-20 10:12:16 -0500

    tmuxinator and copilot

--- dot_config/nvim/lua/custom/plugins/lualine.lua
+++ dot_config/nvim/lua/custom/plugins/lualine.lua
@@ -27,5 +27,30 @@ return {
         }
       }
     }
+    -- Copilot integration
+    -- table.insert(opts.sections.lualine_x, 2, {
+    --   function()
+    --     local icon = LazyVim.config.icons.kinds.Copilot
+    --     local status = require("copilot.api").status.data
+    --     return icon .. (status.message or "")
+    --   end,
+    --   cond = function()
+    --     if not package.loaded["copilot"] then
+    --       return
+    --     end
+    --     local ok, clients = pcall(LazyVim.lsp.get_clients, { name = "copilot", bufnr = 0 })
+    --     if not ok then
+    --       return false
+    --     end
+    --     return ok and #clients > 0
+    --   end,
+    --   color = function()
+    --     if not package.loaded["copilot"] then
+    --       return
+    --     end
+    --     local status = require("copilot.api").status.data
+    --     return colors[status.status] or colors[""]
+    --   end,
+    -- })
   end
 }
