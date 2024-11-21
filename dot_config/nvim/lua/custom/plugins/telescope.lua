1658bb8e93d0decb6b5969832a4f0e400593a10a
Nicolas Arias <elbunuelo@gmail.com>
2024-11-20 21:42:58 -0500

    Add buffer search keymap

--- dot_config/nvim/lua/custom/plugins/telescope.lua
+++ dot_config/nvim/lua/custom/plugins/telescope.lua
@@ -1,5 +1,6 @@
 -- Additional keymaps for telescope, Ctrl + p and \ for searching files and grepping, respectively
 vim.keymap.set('n', '<C-p>', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '\\', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>b', require('telescope.builtin').buffers, { desc = '[S]earch by [G]rep' })
 return {
 }
